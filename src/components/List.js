8ffd952e5718f6cec3c3429595471a4c0f5979ee
Nguyen Van Phuoc <phuocdaicongtuoc@gmail.com>
2019-05-28 00:41:47 +0700

    bug toggle word

--- src/components/List.js
+++ src/components/List.js
@@ -11,6 +11,16 @@ export default class List extends Component {
             ]
         }
     }
+    toggleWord(id){
+        const newWord=this.state.word.map(w=>{
+            if(id===w.id){
+                return {...w, isMemorized:!w.isMemorized}
+            }else {
+                return w;
+            }
+        } )
+        this.setState({word:newWord});
+    }
     render() {
         return (
             <div>
@@ -24,17 +34,17 @@ export default class List extends Component {
                     </div>
                     <div className="btn-container">
                     <button
+                    //neu w.id !==word.id ma khong lam gi ca thi tao ra mot object moi co thuoc tinh word la mang co 1
+                    //phan tu phuong thuc render chay lai thi tai sao lai khong truy cap duoc vao thuoc tinh word.en
+
+                    //giai thich tai vi khi phuong thuc khong tra ve gia ca thi mac dinh tra ve undefined
+                    //nen khi truy cap vao thuoc tinh en cua undefine thi se bi loi
+
+
                         className={word.isMemorized ? 'btn btn-success' : 'btn btn-danger'}
-                        onClick={()=>{
-                            const newWord=this.state.word.map(w=>{
-                                if(w.id===word.id){
-                                    return {...w, isMemorized:!w.isMemorized}
-                                }else{
-                                    return w;
-                                }
-                            } )
-                            this.setState({word:newWord});
-                        }}
+                        onClick={()=>this.toggleWord(word.id)}
+
+                        //tại sao onClick ={this.toggleWord(word.id)} không được mà  onClick={()=>this.toggleWord(word.id)}
                         >
                         {word.isMemorized ? 'Forgot' : 'Memorized'}
                     </button>
